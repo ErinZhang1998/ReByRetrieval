e586612c2e80da21594675e81c88e7adb246911b
ErinZhang1998 <xiaoyuz1@andrew.cmu.edu>
2021-09-10 23:25:45 -0400

    Added self defined mapper, but not working for real-world data yet

---
+++ detectron_mapper.py
@@ -0,0 +1,108 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import copy
+import logging
+import numpy as np
+from typing import List, Optional, Union
+import torch
+
+from kornia import augmentation as K
+
+from detectron2.config import configurable
+from detectron2.data import detection_utils as utils
+from detectron2.data import transforms as T
+
+__all__ = ["RetrievalMapper"]
+
+class RetrievalMapper:
+    def __init__(self, cfg, is_train = True):
+        # augs = utils.build_augmentation(cfg, is_train)
+        augs = [
+            T.RandomBrightness(0.8, 1.8),
+            T.RandomContrast(0.6, 1.3),
+            T.RandomSaturation(0.8, 1.4),
+            T.RandomLighting(0.7),
+            T.RandomFlip(prob=0.4, horizontal=True, vertical=False),
+        ]
+        self.is_train               = is_train
+        self.augmentations          = T.AugmentationList(augs)
+        self.image_format           = cfg.INPUT.FORMAT
+        self.use_instance_mask      = cfg.MODEL.MASK_ON
+        self.instance_mask_format   = cfg.INPUT.MASK_FORMAT
+        self.use_keypoint           = cfg.MODEL.KEYPOINT_ON
+        self.keypoint_hflip_indices = None
+        self.proposal_topk          = None
+        self.recompute_boxes        = False
+    
+    def __call__(self, dataset_dict):
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        image = utils.read_image(dataset_dict["file_name"], format=self.image_format)
+        utils.check_image_size(dataset_dict, image)
+
+        aug_input = T.AugInput(image, sem_seg=None)
+        transforms = self.augmentations(aug_input)
+        image, sem_seg_gt = aug_input.image, aug_input.sem_seg
+
+        image_shape = image.shape[:2]  # h, w
+        dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose(2, 0, 1)))
+
+        if not self.is_train:
+            dataset_dict.pop("annotations", None)
+            dataset_dict.pop("sem_seg_file_name", None)
+            return dataset_dict
+
+        if "annotations" in dataset_dict:
+            annos = [
+                utils.transform_instance_annotations(
+                    obj, transforms, image_shape, keypoint_hflip_indices=self.keypoint_hflip_indices
+                )
+                for obj in dataset_dict.pop("annotations")
+                if obj.get("iscrowd", 0) == 0
+            ]
+            instances = utils.annotations_to_instances(
+                annos, image_shape, mask_format=self.instance_mask_format
+            )
+            dataset_dict["instances"] = utils.filter_empty_instances(instances)
+        
+        return dataset_dict
+
+
+# class MyColorAugmentation(T.Augmentation):
+#     def __init__(self, args):
+#         super().__init__()
+#         self.args = args
+
+#         if type(self.args.dataset_config.color_jitter.brightness) is list:
+#             a,b = self.args.dataset_config.color_jitter.brightness
+#             brightness = (a,b)
+#         else:
+#             brightness = self.args.dataset_config.color_jitter.brightness
+#         if type(self.args.dataset_config.color_jitter.contrast) is list:
+#             a,b = self.args.dataset_config.color_jitter.contrast
+#             contrast = (a,b)
+#         else:
+#             contrast = self.args.dataset_config.color_jitter.contrast
+#         if type(self.args.dataset_config.color_jitter.saturation) is list:
+#             a,b = self.args.dataset_config.color_jitter.saturation
+#             saturation = (a,b)
+#         else:
+#             saturation = self.args.dataset_config.color_jitter.saturation
+#         if type(self.args.dataset_config.color_jitter.hue) is list:
+#             a,b = self.args.dataset_config.color_jitter.hue
+#             hue = (a,b)
+#         else:
+#             hue = self.args.dataset_config.color_jitter.hue
+#         self.color_jitter_prob = self.args.dataset_config.color_jitter.prob
+#         self.color_jitter_transform = K.ColorJitter(
+#             brightness=brightness, 
+#             contrast=contrast, 
+#             saturation=saturation, 
+#             hue=hue,
+#             p=self.args.dataset_config.color_jitter.p,
+#         )
+        
+#         self._init(locals())
+    
+    
+#     def get_transform(self, image):
+#         r = np.random.rand(2)
+#         return T.ColorTransform(lambda x: x * r[0] + r[1] * 10)
--- train_detectron.py
+++ train_detectron.py
@@ -8,20 +8,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pycocotools.mask as coco_mask
 
-
+import utils.logging as logging
 import utils.utils as uu
 import incat_dataset
 import utils.perch_utils as p_utils
 import utils.blender_proc_utils as bp_utils
+from detectron_mapper import RetrievalMapper
 
 # check pytorch installation: 
 import torch, torchvision
-print(torch.__version__, torch.cuda.is_available())
 assert torch.__version__.startswith("1.9")   
-
-# Some basic setup:
-# Setup detectron2 logger
-import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
@@ -33,7 +29,38 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.structures import BoxMode
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
+from detectron2.data import build_detection_train_loader, build_detection_test_loader
+from detectron2.modeling import build_model
+from collections import OrderedDict
+import torch
+from torch.nn.parallel import DistributedDataParallel
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer, PeriodicCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import (
+    MetadataCatalog,
+    build_detection_test_loader,
+    build_detection_train_loader,
+)
+from detectron2.engine import default_argument_parser, default_setup, default_writers, launch
+from detectron2.evaluation import (
+    CityscapesInstanceEvaluator,
+    CityscapesSemSegEvaluator,
+    COCOEvaluator,
+    COCOPanopticEvaluator,
+    DatasetEvaluators,
+    LVISEvaluator,
+    PascalVOCDetectionEvaluator,
+    SemSegEvaluator,
+    inference_on_dataset,
+    print_csv_format,
+)
+from detectron2.modeling import build_model
+from detectron2.solver import build_lr_scheduler, build_optimizer
+from detectron2.utils.events import EventStorage
+
+logger = logging.get_logger(__name__)
 
 idx_to_name = {
     0: 'mug-cylinder',
@@ -78,8 +105,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--config_file", dest="config_file")
 parser.add_argument("--experiment_save_dir", dest="experiment_save_dir")
 parser.add_argument("--experiment_save_dir_default", dest="experiment_save_dir_default")
+parser.add_argument("--init_method", dest="init_method", default="tcp://localhost:9999",type=str)
 
+'''
+Load the image
+Apply transform
+Get back to numpy array? 
 
+'''
 
 def load_annotations_detectron(args, split, df, one_scene_dir):
     scene_num = int(one_scene_dir.split('/')[-1].split('_')[-1])
@@ -142,7 +175,6 @@ def get_data_detectron(args, split):
     else:
         raise
 
-    print(scene_dir)
     dir_list = uu.data_dir_list(
         scene_dir, 
         must_contain_file = ['0.hdf5', 'coco_data/coco_annotations.json']
@@ -161,22 +193,16 @@ def get_data_detectron(args, split):
     return data_dict_lists
 
 
-def main(options, args):
-    for split in ["train", "test"]:
-        DatasetCatalog.register(
-            "blender_proc_dataset_" + split, 
-            lambda split = split: get_data_detectron(args, split),
-        )
-        MetadataCatalog.get("blender_proc_dataset_" + split).thing_classes = list(idx_to_name.values())
-    
-    if args.wandb_detectron.enable:
-        wandb.login()
-        wandb.init(
-            project=args.wandb_detectron.wandb_project_name, 
-            entity=args.wandb_detectron.wandb_project_entity, 
-            config=args.obj_dict,
-        )
-    
+def setup(options, args):
+    if comm.is_main_process():
+        if args.wandb_detectron.enable:
+            wandb.login()
+            wandb.init(
+                project=args.wandb_detectron.wandb_project_name, 
+                entity=args.wandb_detectron.wandb_project_entity, 
+                config=args.obj_dict,
+            )
+        
     wandb_enabled = args.wandb_detectron.enable and not wandb.run is None
     if wandb_enabled:
         wandb_run_name = wandb.run.name 
@@ -184,46 +210,137 @@ def main(options, args):
         wandb_run_name = uu.get_timestamp()
 
     if options.experiment_save_dir is None:
-        uu.create_dir(options.experiment_save_dir_default)
-        this_experiment_dir = os.path.join(options.experiment_save_dir_default, wandb_run_name)
+        if comm.is_main_process():
+            uu.create_dir(options.experiment_save_dir_default)
+        experiment_save_dir = os.path.join(options.experiment_save_dir_default, wandb_run_name)
     else:
-        this_experiment_dir = options.experiment_save_dir
-    uu.create_dir(this_experiment_dir)
+        experiment_save_dir = options.experiment_save_dir
+    if comm.is_main_process():
+        uu.create_dir(experiment_save_dir)
+        logging.setup_logging(log_to_file=args.log_to_file, experiment_dir=experiment_save_dir)
     
-    print("cfg.OUTPUT_DIR: ", this_experiment_dir)
+    logger.info("cfg.OUTPUT_DIR: {}".format(experiment_save_dir))
+    
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     cfg.DATASETS.TRAIN = ("blender_proc_dataset_train",)
     cfg.DATASETS.TEST = ()
-    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.DATALOADER.NUM_WORKERS = 10
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  
-    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.IMS_PER_BATCH = 128
     cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
     cfg.SOLVER.MAX_ITER = 10000    
     cfg.SOLVER.STEPS = []        # do not decay learning rate
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512  
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(idx_to_name)  
     cfg.INPUT.MASK_FORMAT = 'bitmask'
-    cfg.OUTPUT_DIR = this_experiment_dir
+    cfg.OUTPUT_DIR = experiment_save_dir
 
-    trainer = DefaultTrainer(cfg) 
-    trainer.resume_or_load(resume=False)
-    trainer.train()
+    return cfg
 
-    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.2   # set a custom testing threshold
-    predictor = DefaultPredictor(cfg)
-    evaluator = COCOEvaluator("blender_proc_dataset_test", output_dir=this_experiment_dir)
-    val_loader = build_detection_test_loader(cfg, "blender_proc_dataset_test")
-    print(inference_on_dataset(predictor.model, val_loader, evaluator))
-    # another equivalent way to evaluate the model is to use `trainer.test`
+def do_train(cfg, model, resume=False):
+    model.train()
+    optimizer = build_optimizer(cfg, model)
+    scheduler = build_lr_scheduler(cfg, optimizer)
+
+    checkpointer = DetectionCheckpointer(
+        model, cfg.OUTPUT_DIR, optimizer=optimizer, scheduler=scheduler
+    )
+    start_iter = (
+        checkpointer.resume_or_load(cfg.MODEL.WEIGHTS, resume=resume).get("iteration", -1) + 1
+    )
+    max_iter = cfg.SOLVER.MAX_ITER
+
+    periodic_checkpointer = PeriodicCheckpointer(
+        checkpointer, cfg.SOLVER.CHECKPOINT_PERIOD, max_iter=max_iter
+    )
+
+    writers = default_writers(cfg.OUTPUT_DIR, max_iter) if comm.is_main_process() else []
+
+    data_loader = build_detection_train_loader(cfg, mapper=RetrievalMapper(cfg, is_train=True))
+    logger.info("Starting training from iteration {}".format(start_iter))
+    with EventStorage(start_iter) as storage:
+        for data, iteration in zip(data_loader, range(start_iter, max_iter)):
+            storage.iter = iteration
+
+            loss_dict = model(data)
+            losses = sum(loss_dict.values())
+            assert torch.isfinite(losses).all(), loss_dict
+
+            loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
+            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+            if comm.is_main_process():
+                storage.put_scalars(total_loss=losses_reduced, **loss_dict_reduced)
+
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
+            storage.put_scalar("lr", optimizer.param_groups[0]["lr"], smoothing_hint=False)
+            scheduler.step()
+
+            # if (
+            #     cfg.TEST.EVAL_PERIOD > 0
+            #     and (iteration + 1) % cfg.TEST.EVAL_PERIOD == 0
+            #     and iteration != max_iter - 1
+            # ):
+            #     do_test(cfg, model)
+            #     # Compared to "train_net.py", the test results are not dumped to EventStorage
+            #     comm.synchronize()
+
+            if iteration - start_iter > 5 and (
+                (iteration + 1) % 20 == 0 or iteration == max_iter - 1
+            ):
+                for writer in writers:
+                    writer.write()
+            periodic_checkpointer.step(iteration)
+
+
+def main(options, args):
+    cfg = setup(options, args)
+    model = build_model(cfg)
+    distributed = comm.get_world_size() > 1
+    # logger.info("Model:\n{}".format(model))
+    if distributed:
+        model = DistributedDataParallel(
+            model, device_ids=[comm.get_local_rank()], broadcast_buffers=False
+        )
+    if comm.is_main_process():
+        for split in ["train", "test"]:
+            DatasetCatalog.register(
+                "blender_proc_dataset_" + split, 
+                lambda split = split: get_data_detectron(args, split),
+            )
+            MetadataCatalog.get("blender_proc_dataset_" + split).thing_classes = list(idx_to_name.values())
+        
+    
+    do_train(cfg, model, resume=args.resume)
+    
+    
+    # trainer = DefaultTrainer(cfg) 
+    # trainer.resume_or_load(resume=False)
+    # trainer.train()
+
+    # cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+    # cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.2   # set a custom testing threshold
+    # predictor = DefaultPredictor(cfg)
+    # evaluator = COCOEvaluator("blender_proc_dataset_test", output_dir=this_experiment_dir)
+    # val_loader = build_detection_test_loader(cfg, "blender_proc_dataset_test")
+    # print(inference_on_dataset(predictor.model, val_loader, evaluator))
+    # # another equivalent way to evaluate the model is to use `trainer.test`
 
 if __name__ == '__main__':
     options = parser.parse_args()
     f =  open(options.config_file)
     args_dict = yaml.safe_load(f)
-    default_args_dict = yaml.safe_load(open('configs/default.yaml'))
-    args_dict_filled = uu.fill_in_args_from_default(args_dict, default_args_dict)
-    args = uu.Struct(args_dict_filled)
+    # default_args_dict = yaml.safe_load(open('configs/default.yaml'))
+    # args_dict = uu.fill_in_args_from_default(args_dict, default_args_dict)
+    config_args = uu.Struct(args_dict)
 
-    main(options, args)
+    launch(
+        main,
+        4,
+        num_machines=1,
+        machine_rank=0,
+        dist_url=options.init_method,
+        args=(options,config_args),
+    )
